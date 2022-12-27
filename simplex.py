542a922bccaa2c0c87f8a8e8ca32eab24a7ae91d
Arya Prasetya <arya.prasetya@tu-berlin.de>
2022-12-27 02:10:03 +0100

    add auxillary problem generator

--- simplex.py
+++ simplex.py
@@ -5,14 +5,13 @@ class Problem:
         self.A=A
         self.b=b
         self.c=c
+        assert(np.shape(A)==(np.shape(b)[0],np.shape(c)[0])), "Matrix size did not coincide objective and constraint vector."
         self.x=np.zeros(np.shape(A)[1])
 
     def __str__(self) -> str:
         return f"===LP Solver===\nObjective : {self.c}\nConstraints :\n--A--\n{self.A}\n--b--\n{self.b}\n--x--\n{self.x}\n============"
 
     def transform_problem(self):
-        A=self.A
-        b=self.b
         '''
         Transforms constraints in problem such that b>=0 
 
@@ -23,11 +22,30 @@ class Problem:
         b : ndarray(m,)
             constraint vector
         '''
+        A=self.A
+        b=self.b
+
         assert(np.shape(A)[0]==np.shape(b)[0])
         for i,bi in enumerate(b):
             if bi<0:
                 A[i,:]=-A[i,:]
                 b[i]=-bi
+    
+    def auxillary_problem(self):
+        '''
+        Generate auxillary problem
+        '''
+        A=self.A
+        b=self.b
+        c=self.c
+        m,n=np.shape(A)
+
+        aux_prob = Problem(
+            np.append(A,np.diag(np.ones(m)),axis=1),
+            b,
+            np.append(np.zeros(n),np.ones(m))
+            )
+        return aux_prob
 
 def simplex_method(c,A,b):
     '''
